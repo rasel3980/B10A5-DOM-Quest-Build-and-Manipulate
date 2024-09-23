1a05654b3f8b82ce4136375c65dbe83026248545
rasel3980 <rkrasel3980@gmail.com>
2024-09-23 18:28:41 +0600

    Donate full part complete

--- js/donate.js
+++ js/donate.js
@@ -1,1 +1,13 @@
-console.log('Donate Bangladesh');
+// Button Color
+const colorDonation =document.getElementById('donation-btn-color');
+        colorDonation.addEventListener('click', function(){
+            colorDonation.style.backgroundColor = '#B4F461';
+            
+        })
+        const colorHistory =document.getElementById('history-btn-color');
+        colorHistory.addEventListener('click', function(e){
+            e.preventDefault();
+            this.classList.add('#B4F461');
+            // colorHistory.style.backgroundColor = '#B4F461';
+            window.location.href = 'history.html'
+        })
